caaadb6aa0fae9c359b3b18f435fc0899074b7fb
tg00132 <tg00132@naver.com>
2021-02-08 16:41:16 +0900

    0208class

---
+++ 20210208/class0208/class0208/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class0208
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student st = new Student();
+            st.setName("홍길동");
+            st.setTel("010-1234-5678");
+            st.printStudentInfo();
+            Console.WriteLine("이름: " + st.getName());
+
+            Student2 st2 = new Student2();
+            st2.Name = "김길동";
+            st2.Tel = "010-1234-4321";
+            st2.printStudentinfo();
+            Console.WriteLine("이름 : " + st2.Name);
+
+            Student2 st3 = new Student2("최길동", "010-2222-2222");
+            st3.printStudentinfo();
+
+            new Student2("최길동", "010-2222-2222").printStudentinfo();
+
+            string[] nameArr = { "홍길동", "김길동", "박길동" };
+            string[] telArr = { "010-1111-1111", "010-2222-2222", "010-3333-3333" };
+            Random r = new Random();
+            Student2[] stArr = new Student2[10];
+            for(int i=0; i<stArr.Length; i++)
+            {
+                // Next(0,3) : 0~2까지 숫자 발생
+                stArr[i] = new Student2(nameArr[r.Next(0, 3)],telArr[r.Next(0,3)]);
+                stArr[i].printStudentinfo();
+            }
+            //클래스 변수 사용
+            Console.WriteLine("과정명: " + Student2.CLASS);
+        }
+    }
+
+    class Student
+    {
+        //캡슐화
+        private string name;
+        private string tel;
+
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+        public string getName()
+        {
+            return name;
+        }
+        public void setTel(string tel)
+        {
+            this.tel = tel;
+        }
+        public string getTel()
+        {
+            return tel;
+        }
+
+        public void printStudentInfo()
+        {
+            Console.WriteLine("이름 : " + name);
+            Console.WriteLine("전화번호 : " + tel);
+        }
+    }
+}
