2b66572bc4c6ffe721e3c5450adff6aebe08e276
tg00132 <tg00132@naver.com>
2021-02-15 09:29:24 +0900

    20210208 C#

---
+++ 20210208/Car0208/Car0208/Car.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car0208
+{
+    class Car
+    {
+        private string model;
+        private string company;
+        private string price;
+        private string color;
+
+        //클래스 상수 변수 
+         const string agency = "동대구 영업소";
+
+        public void setModel(string model)
+        {
+            this.model = model;
+        }
+        public string getModel()
+        {
+            return model;
+        }
+        public void setCompany(string company)
+        {
+            this.company = company;
+        }
+        public string getCompany()
+        {
+            return company;
+        }
+        public void setPrice(string price)
+        {
+            this.price = price;
+        }
+        public string getPrice()
+        {
+            return price;
+        }
+        public void setColor(string color)
+        {
+            this.color = color;
+        }
+        public string getColor()
+        {
+            return color;
+        }
+
+        public void showCarInfo()
+        {
+            Console.WriteLine("대리점 영업소: " + Car.agency);
+            Console.WriteLine("모델명: " + model);
+            Console.WriteLine("가격: " + price);
+            Console.WriteLine("색상: " + color);
+            Console.WriteLine("제조사: " + company);
+        }
+
+    }
+}
---
+++ 20210208/Car0208/Car0208/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car0208
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.setModel("K7");
+            car.setPrice("사천만원");
+            car.setColor("블랙");
+            car.setCompany("기아");
+            car.showCarInfo();
+
+            
+
+        }
+    }
+}
